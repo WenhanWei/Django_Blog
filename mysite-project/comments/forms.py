5cd68f433c9840216ff87674d8762c26e74777ce
WenhanWei <weiwenhan0702@outlook.com>
2020-07-29 09:32:25 +0800

    fix comment text area no showing problem

--- mysite-project/comments/forms.py
+++ mysite-project/comments/forms.py
@@ -1,12 +1,8 @@
 from django import forms
 from .models import Comment
-from django_summernote.widgets import SummernoteWidget
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'text']
-        widgets = {
-            'text': SummernoteWidget(),
-        }
